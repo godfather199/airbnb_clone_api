72c7649c28c3989f1fd33aae2bd27ca6612cdac3
¨godfather199¨ <rajatmehra21@gmail.com>
2024-06-26 13:37:13 +0530

    New update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({type: '10mb', extended: 'true'}))
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://airbnb-clone-api-1evg.onrender.com',
+    origin: 'https://airbnb-clone-client-t8vm.onrender.com/',
     credentials: true
 }))
 
